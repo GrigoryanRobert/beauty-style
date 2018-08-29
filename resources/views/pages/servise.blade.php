0e754d343a1f071cb8353debfb4109e982b9f661
Robert Grigoryan <youngman87@mail.ru>
2018-08-29 17:39:40 +0400

    upload5

---
+++ resources/views/pages/servise.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.layoutservise')
+
+@section('content')
+
+        <!-- services -->
+<div class="services-agile-w3l userservise" id="services">
+
+    <h2 class="tittle-w3l">{{Auth::user()->name }} Registracia</h2>
+
+
+
+
+
+</div>
+
+<!-- //services -->
+
+@endsection
